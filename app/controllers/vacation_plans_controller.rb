87f5f6e6771c42b7833b33b2ef872f13751dce81
JeffEckert <bromodo@gmail.com>
2020-07-03 21:18:47 -0400

    added the create action to controller and tested is working.

--- app/controllers/vacation_plans_controller.rb
+++ app/controllers/vacation_plans_controller.rb
@@ -9,7 +9,18 @@ class VacationPlansController < ApplicationController
     end
 
     def create
+        @vacation_plans = VacationPlan.new(vacation_plan_params)
+            if @vacation_plans.save
+                redirect_to new_vacation_plan_park_day_path(@vacation_plans)
+            else
+                render :new
+            end
+    end
 
+    private
+
+    def vacation_plan_params
+        params.require(:vacation_plan).permit(:name)
     end
 
 
--- db/local_seed.rb
+++ db/local_seed.rb
@@ -1,7 +1,15 @@
+require 'json'
 Attraction.destroy_all
 
 
 
-JSON.parse(File.read("disney_attractions.json")).each do |attraction_attrs|
+# JSON.parse(File.read("disney_attractions.json")).each do |attraction_attributes|
+#     Attraction.create(attraction_attributes)
+# end
+
+file = File.read('disney_attractions.json')
+data_hash = JSON.parse(file)
+
+data_hash.each do |attraction_attrs|
     Attraction.create(attraction_attrs)
 end
