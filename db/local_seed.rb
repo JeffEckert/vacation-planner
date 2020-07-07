54385f74bfd9e638f5e50f4cc96e4cc27d2b3c9d
JeffEckert <bromodo@gmail.com>
2020-07-07 19:17:41 -0400

    made changes to assist in seeding data to attractions

--- db/local_seed.rb
+++ db/local_seed.rb
@@ -5,15 +5,7 @@ VacationPlan.destroy_all
 
 
 
-# JSON.parse(File.read("disney_attractions.json")).each do |attraction_attributes|
-#     Attraction.create(attraction_attributes)
-# end
-
-file = File.read('disney_attractions.json')
-# binding.pry
-json = JSON.parse(file)
-
-binding.pry
-json.each do |attractions_hash|
-    Attraction.create(attractions_hash = {})
+JSON.parse(File.read("disney_attractions.json")).each do |attraction_attributes|
+    Attraction.create(attraction_attributes)
 end
+
