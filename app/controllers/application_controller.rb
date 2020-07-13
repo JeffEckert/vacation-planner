46c9b73e5c5ca5e7a6128b6c40826ddd9df001b9
JeffEckert <bromodo@gmail.com>
2020-07-12 21:02:14 -0400

    added some stylesheet and navigation options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
     def current_user
         @user = User.find_by(id: session[:user_id])
     end
@@ -15,7 +14,4 @@ class ApplicationController < ActionController::Base
     def log_in(user) 
         session[:user_id] = user.id
     end
-
-
-
 end
